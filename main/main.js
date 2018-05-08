6af755e9b6b721d675ac0a25c4bc3616883c0a94
lajimian <472905899@qq.com>
2018-05-08 10:28:06 +0800

    完成出租车计价

--- main/main.js
+++ main/main.js
@@ -1,4 +1,10 @@
-module.exports = function main() {
-    console.log("Debug Info");
-    return 'Hello World!';
+module.exports = function main(mileage,wait) {
+    var price;
+    if(mileage<=2)
+        price =Math.round(6 + wait*0.25) ;
+    else if(mileage<=8)
+        price = Math.round(6+0.8*(mileage-2)+wait*0.25);
+    else
+        price = Math.round(6+0.8*(8-2)+0.8*1.5*(mileage-8)+wait*0.25);
+    return price;
 };
--- spec/main-spec.js
+++ spec/main-spec.js
@@ -1,5 +1,48 @@
 const main = require('../main/main');
 
 describe('taxi fee', function () {
-    // write your tests here...
+    it('should generate price when the actual mileage is 两公里内', function () {
+        let mileage = 1.5;
+        let price = main(mileage, 0);
+        let expected = 6;
+        expect(price).toEqual(expected);
+    });
+
+    it('should generate price when the actual mileage is 两公里外八公里内', function () {
+        let mileage = 6;
+        let price = main(mileage, 0);
+        let expected = 9;
+        expect(price).toEqual(expected);
+    });
+
+    it('should generate price when the actual mileage is 八公里外', function () {
+        let mileage = 10;
+        let price = main(mileage, 0);
+        let expected = 13;
+        expect(price).toEqual(expected);
+    });
+
+    it('should generate price when the actual mileage is 两公里内有停车', function () {
+        let mileage = 1.5;
+        let wait = 10;
+        let price = main(mileage, wait);
+        let expected = 9;
+        expect(price).toEqual(expected);
+    });
+
+    it('should generate price when the actual mileage is 两公里外八公里内有停车', function () {
+        let mileage = 6;
+        let wait = 10;
+        let price = main(mileage, wait);
+        let expected = 12;
+        expect(price).toEqual(expected);
+    });
+
+    it('should generate price when the actual mileage is 八公里外有停车', function () {
+        let mileage = 10;
+        let wait = 10;
+        let price = main(mileage, wait);
+        let expected = 16;
+        expect(price).toEqual(expected);
+    });
 });
